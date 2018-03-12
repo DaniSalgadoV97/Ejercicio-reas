f275d5d6f8ac9a6a12f5a919915c479900d90252
Estudiantes <Estudiantes@Labing70201>
2018-03-12 17:20:51 -0500

    segundo commit

---
+++ Áreas/src/áreas/Triángulo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package áreas;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Triángulo extends Figurageométrica {
+
+    @Override
+    double hallararea() {
+        double x = 0, y = 0, a;
+        a = x * y;
+        return 0;
+
+    }
+
+    @Override
+    double hallarperimetro() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
---
+++ Áreas/src/áreas/Áreas.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package áreas;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Áreas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Rectángulo miRectángulo = new Rectángulo();
+        System.out.println(miRectángulo.hallararea());
+
+        // TODO code application logic here
+    }
+    
+}
